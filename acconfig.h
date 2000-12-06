96d2e33447d9ba804dd4be5ba0911da7b05840b1
Andrew Hunter <andrew@logicalshift.co.uk>
2000-12-06 20:08:32 +0000

    Merged windows changes
    --HG--
    branch : zoom
    extra : convert_revision : svn%3Af2cc7feb-2fdc-0310-9003-87a4627787f1/trunk/zoom%40115

--- acconfig.h
+++ acconfig.h
@@ -1,2 +1,8 @@
 /* Which parser to use for command line options */
 #undef OPT_TYPE
+
+/* Which window system to use */
+#undef WINDOW_SYSTEM
+
+/* Do we have gettimeofday? */
+#undef HAVE_GETTIMEOFDAY
