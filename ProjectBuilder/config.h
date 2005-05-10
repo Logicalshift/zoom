c2c2b7f5ba02ea9f19d4ed17800a62c42680a34c
Andrew Hunter <andrew@logicalshift.co.uk>
2005-05-10 12:43:55 +0000

    - updated version to remove 'beta' moniker: preparation for final 1.0.2    release.
    --HG--
    branch : zoom
    extra : convert_revision : svn%3Af2cc7feb-2fdc-0310-9003-87a4627787f1/trunk/zoom%401697

--- ProjectBuilder/config.h
+++ ProjectBuilder/config.h
@@ -84,7 +84,7 @@
 #define STDC_HEADERS 1
 
 /* Version number of package */
-#define VERSION "1.0.2beta2"
+#define VERSION "1.0.2"
 
 /* Define if the X Window System is missing or not being used. */
 /* #undef X_DISPLAY_MISSING */
