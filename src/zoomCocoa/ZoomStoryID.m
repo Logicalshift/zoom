9c8779d3964756a07971497ad33c8c28cd65e5b2
Andrew Hunter <andrew@logicalshift.co.uk>
2006-04-20 22:21:25 +0000

    - cleared up the things that were stopping ZoomStoryID from compiling
    --HG--
    branch : zoom
    extra : convert_revision : svn%3Af2cc7feb-2fdc-0310-9003-87a4627787f1/trunk/zoom%402002

--- src/zoomCocoa/ZoomStoryID.m
+++ src/zoomCocoa/ZoomStoryID.m
@@ -58,47 +58,57 @@
 			[blorbFile release];
 		}
 		
-		// Interpret the Z-Code data into a identification
-		ident = IFID_Alloc();
+		// Interpret the Z-Code data into an identification
 		needsFreeing = YES;
-		
-		ident->format = ident->dataFormat = IFFormat_ZCode;
-
-		memcpy(ident->data.zcode.serial, bytes + 0x12, 6);
-		ident->data.zcode.release  = (((int)bytes[0x2])<<8)|((int)bytes[0x3]);
-		ident->data.zcode.checksum = (((int)bytes[0x1c])<<8)|((int)bytes[0x1d]);
-		ident->usesMd5 = 0;
+		ident = IFMB_ZcodeId((((int)bytes[0x2])<<8)|((int)bytes[0x3]),
+							 bytes + 0x12,
+							 (((int)bytes[0x1c])<<8)|((int)bytes[0x1d]));
 		
 		// Scan for the string 'UUID://' - use this as an ident for preference if it exists (and represents a valid UUID)
-		struct IFMDUUID uuid;
 		int x;
 		BOOL gotUUID = NO;
-		
-		for (x=0; x<16; x++) uuid.uuid[x] = 0;
 		
 		for (x=0; x<length-48; x++) {
 			if (bytes[x] == 'U' && bytes[x+1] == 'U' && bytes[x+2] == 'I' && bytes[x+3] == 'D' &&
 				bytes[x+4] == ':' && bytes[x+5] == '/' && bytes[x+6] == '/') {
 				// This might be a UUID section
-				uuid = IFMD_ReadUUID((char*)(bytes + x + 7));
+				char uuidText[49];
 				
 				// Check to see if we've got a UUID
 				int y;
-				gotUUID = NO;
+				int digitCount = 0;
+				gotUUID = YES;
 				
-				for (y=0; y<16; y++) {
-					if (uuid.uuid[y] != 0) gotUUID = YES;
+				for (y=0; y<7; y++) uuidText[y] = bytes[x+y];
+				for (y=7; y<48; y++) {
+					uuidText[y] = bytes[x+y];
+					
+					if (bytes[x+y-1] == '/' && bytes[x+y] == '/') break;
+					if (bytes[x+y] == '-' || bytes[x+y] == '/') continue;
+					if ((bytes[x+y] >= '0' && bytes[x+y] <= '9') ||
+						(bytes[x+y] >= 'a' && bytes[x+y] <= 'f') ||
+						(bytes[x+y] >= 'A' && bytes[x+y] <= 'F')) {
+						digitCount++;
+						continue;
+					}
+					
+					gotUUID = NO;
+					break;
 				}
 				
+				if (gotUUID) {
+					IFID uuidId = IFMB_IdFromString(uuidText);
+					
+					if (uuidId == NULL) {
+						gotUUID = false;
+					} else {
+						IFMB_FreeId(ident);
+						ident = uuidId;
+					}
+				}
+
 				if (gotUUID) break;
 			}
-		}
-		
-		if (gotUUID) {
-			// We've got a UUID to use
-			ident->dataFormat = IFFormat_UUID;
-			ident->data.uuid = uuid;
-			ident->usesMd5 = 0;
 		}
 	}
 	
@@ -163,47 +173,57 @@
 			return nil;
 		}
 		
-		// Read the ID from the Z-Code data
-		ident = IFID_Alloc();
+		// Interpret the Z-Code data into an identification
 		needsFreeing = YES;
-		
-		ident->format = ident->dataFormat = IFFormat_ZCode;
-		
-		memcpy(ident->data.zcode.serial, bytes + 0x12, 6);
-		ident->data.zcode.release  = (((int)bytes[0x2])<<8)|((int)bytes[0x3]);
-		ident->data.zcode.checksum = (((int)bytes[0x1c])<<8)|((int)bytes[0x1d]);
-		ident->usesMd5 = 0;
+		ident = IFMB_ZcodeId((((int)bytes[0x2])<<8)|((int)bytes[0x3]),
+							 bytes + 0x12,
+							 (((int)bytes[0x1c])<<8)|((int)bytes[0x1d]));
 		
 		// Scan for the string 'UUID://' - use this as an ident for preference if it exists (and represents a valid UUID)
-		struct IFMDUUID uuid;
 		int x;
 		BOOL gotUUID = NO;
-		
-		for (x=0; x<16; x++) uuid.uuid[x] = 0;
 		
 		for (x=0; x<length-48; x++) {
 			if (bytes[x] == 'U' && bytes[x+1] == 'U' && bytes[x+2] == 'I' && bytes[x+3] == 'D' &&
 				bytes[x+4] == ':' && bytes[x+5] == '/' && bytes[x+6] == '/') {
 				// This might be a UUID section
-				uuid = IFMD_ReadUUID((char*)(bytes + x + 7));
+				char uuidText[49];
 				
 				// Check to see if we've got a UUID
 				int y;
-				gotUUID = NO;
+				int digitCount = 0;
+				gotUUID = YES;
 				
-				for (y=0; y<16; y++) {
-					if (uuid.uuid[y] != 0) gotUUID = YES;
+				for (y=0; y<7; y++) uuidText[y] = bytes[x+y];
+				for (y=7; y<48; y++) {
+					uuidText[y] = bytes[x+y];
+					
+					if (bytes[x+y-1] == '/' && bytes[x+y] == '/') break;
+					if (bytes[x+y] == '-' || bytes[x+y] == '/') continue;
+					if ((bytes[x+y] >= '0' && bytes[x+y] <= '9') ||
+						(bytes[x+y] >= 'a' && bytes[x+y] <= 'f') ||
+						(bytes[x+y] >= 'A' && bytes[x+y] <= 'F')) {
+						digitCount++;
+						continue;
+					}
+					
+					gotUUID = NO;
+					break;
+				}
+				
+				if (gotUUID) {
+					IFID uuidId = IFMB_IdFromString(uuidText);
+					
+					if (uuidId == NULL) {
+						gotUUID = false;
+					} else {
+						IFMB_FreeId(ident);
+						ident = uuidId;
+					}
 				}
 				
 				if (gotUUID) break;
 			}
-		}
-		
-		if (gotUUID) {
-			// We've got a UUID to use
-			ident->dataFormat = IFFormat_UUID;
-			ident->data.uuid = uuid;
-			ident->usesMd5 = 0;
 		}
 	}
 	
@@ -240,7 +260,7 @@
 	[super dealloc];
 }
 
-- (struct IFMDIdent*) ident {
+- (struct IFID*) ident {
 	return ident;
 }
 
@@ -248,8 +268,7 @@
 - (id) copyWithZone: (NSZone*) zone {
 	ZoomStoryID* newID = [[ZoomStoryID allocWithZone: zone] init];
 	
-	newID->ident = IFID_Alloc();
-	IFIdent_Copy(newID->ident, ident);
+	newID->ident = IFMB_CopyId(ident);
 	newID->needsFreeing = YES;
 	
 	return newID;
@@ -258,50 +277,42 @@
 // = NSCoding =
 - (void)encodeWithCoder:(NSCoder *)encoder {
 	// Version might change later on
-	int version = 1;
+	int version = 2;
 	
-	[encoder encodeValueOfObjCType: @encode(int) at: &version];
+	[encoder encodeValueOfObjCType: @encode(int) 
+								at: &version];
 	
-	// General stuff (data format, MD5, etc)
-	[encoder encodeValueOfObjCType: @encode(enum IFMDFormat) 
-								at: &ident->dataFormat];
-	[encoder encodeValueOfObjCType: @encode(IFMDByte)
-								at: &ident->usesMd5];
-	if (ident->usesMd5) {
-		[encoder encodeArrayOfObjCType: @encode(IFMDByte)
-								 count: 16
-									at: ident->md5Sum];
-	}
-	
-	switch (ident->dataFormat) {
-		case IFFormat_ZCode:
-			[encoder encodeArrayOfObjCType: @encode(IFMDByte)
-									 count: 6
-										at: ident->data.zcode.serial];
-			[encoder encodeValueOfObjCType: @encode(int)
-										at: &ident->data.zcode.release];
-			[encoder encodeValueOfObjCType: @encode(int)
-										at: &ident->data.zcode.checksum];
-			break;
-			
-		case IFFormat_UUID:
-			[encoder encodeArrayOfObjCType: @encode(unsigned char)
-									 count: 16
-										at: ident->data.uuid.uuid];
-			break;
-			
-			
-		default:
-			/* No other formats are supported yet */
-			break;
-	}
+	char* stringId = IFMB_IdToString(ident);
+	NSString* stringIdent = [NSString stringWithUTF8String: stringId];
+	[encoder encodeObject: stringIdent];
+	free(stringId);
 }
+
+enum IFMDFormat {
+	IFFormat_Unknown = 0x0,
+	
+	IFFormat_ZCode,
+	IFFormat_Glulx,
+	
+	IFFormat_TADS,
+	IFFormat_HUGO,
+	IFFormat_Alan,
+	IFFormat_Adrift,
+	IFFormat_Level9,
+	IFFormat_AGT,
+	IFFormat_MagScrolls,
+	IFFormat_AdvSys,
+	
+	IFFormat_UUID,			/* 'Special' format used for games identified by a UUID */
+};
+
+typedef unsigned char IFMDByte;
 
 - (id)initWithCoder:(NSCoder *)decoder {
 	self = [super init];
 	
 	if (self) {
-		ident = IFID_Alloc();
+		ident = NULL;
 		needsFreeing = YES;
 		
 		// As above, but backwards
@@ -309,49 +320,68 @@
 		
 		[decoder decodeValueOfObjCType: @encode(int) at: &version];
 		
-		if (version != 1) {
-			// Only v1 decodes supported ATM
+		if (version == 1) {
+			// General stuff (data format, MD5, etc) [old v1 format used by versions of Zoom prior to 1.0.5dev3]
+			char md5sum[16];
+			IFMDByte usesMd5;
+			enum IFMDFormat dataFormat;
+			
+			[decoder decodeValueOfObjCType: @encode(enum IFMDFormat) 
+										at: &dataFormat];
+			[decoder decodeValueOfObjCType: @encode(IFMDByte)
+										at: &usesMd5];
+			if (usesMd5) {
+				[decoder decodeArrayOfObjCType: @encode(IFMDByte)
+										 count: 16
+											at: md5sum];
+			}
+			
+			switch (dataFormat) {
+				case IFFormat_ZCode:
+				{
+					char serial[6];
+					int release;
+					int checksum;
+					
+					[decoder decodeArrayOfObjCType: @encode(IFMDByte)
+											 count: 6
+												at: serial];
+					[decoder decodeValueOfObjCType: @encode(int)
+												at: &release];
+					[decoder decodeValueOfObjCType: @encode(int)
+												at: &checksum];
+					
+					ident = IFMB_ZcodeId(release, serial, checksum);
+					break;
+				}
+					
+				case IFFormat_UUID:
+				{
+					unsigned char uuid[16];
+					
+					[decoder decodeArrayOfObjCType: @encode(unsigned char)
+											 count: 16
+												at: uuid];
+					ident = IFMB_UUID(uuid);
+					break;
+				}
+					
+				default:
+					/* No other formats are supported yet */
+					break;
+			}		
+		} else if (version == 2) {
+			NSString* idString = (NSString*)[decoder decodeObject];
+			
+			ident = IFMB_IdFromString([idString UTF8String]);
+		} else {
+			// Only v1 and v2 decodes supported ATM
 			[self release];
 			
-			NSLog(@"Tried to load a version %i ZoomStoryID (this version of Zoom supports only version 1)", version);
+			NSLog(@"Tried to load a version %i ZoomStoryID (this version of Zoom supports only versions 1 and 2)", version);
 			
 			return nil;
 		}
-		
-		// General stuff (data format, MD5, etc)
-		[decoder decodeValueOfObjCType: @encode(enum IFMDFormat) 
-									at: &ident->dataFormat];
-		ident->format = ident->dataFormat;
-		[decoder decodeValueOfObjCType: @encode(IFMDByte)
-									at: &ident->usesMd5];
-		if (ident->usesMd5) {
-			[decoder decodeArrayOfObjCType: @encode(IFMDByte)
-									 count: 16
-										at: ident->md5Sum];
-		}
-		
-		switch (ident->dataFormat) {
-			case IFFormat_ZCode:
-				[decoder decodeArrayOfObjCType: @encode(IFMDByte)
-										 count: 6
-											at: ident->data.zcode.serial];
-				[decoder decodeValueOfObjCType: @encode(int)
-											at: &ident->data.zcode.release];
-				[decoder decodeValueOfObjCType: @encode(int)
-											at: &ident->data.zcode.checksum];
-				break;
-				
-			case IFFormat_UUID:
-				ident->format = IFFormat_ZCode;
-				[decoder decodeArrayOfObjCType: @encode(unsigned char)
-										 count: 16
-											at: ident->data.uuid.uuid];
-				break;
-				
-			default:
-				/* No other formats are supported yet */
-				break;
-		}		
 	}
 	
 	return self;
@@ -377,52 +407,11 @@
 }
 
 - (NSString*) description {
-	switch (ident->dataFormat) {
-		case IFFormat_ZCode:
-			return [NSString stringWithFormat: @"ZoomStoryID (ZCode): %i.%.6s.%04x",
-				ident->data.zcode.release,
-				ident->data.zcode.serial,
-				ident->data.zcode.checksum];
-			break;
-			
-		case IFFormat_UUID:
-			return [NSString stringWithFormat: @"ZoomStoryID (UUID): %02x%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x",
-				ident->data.uuid.uuid[0],
-				ident->data.uuid.uuid[1],
-				ident->data.uuid.uuid[2],
-				ident->data.uuid.uuid[3],
-				ident->data.uuid.uuid[4],
-				ident->data.uuid.uuid[5],
-				ident->data.uuid.uuid[6],
-				ident->data.uuid.uuid[7],
-				ident->data.uuid.uuid[8],
-				ident->data.uuid.uuid[9],
-				ident->data.uuid.uuid[10],
-				ident->data.uuid.uuid[11],
-				ident->data.uuid.uuid[12],
-				ident->data.uuid.uuid[13],
-				ident->data.uuid.uuid[14],
-				ident->data.uuid.uuid[15]];
-			break;
-			
-		default:
-			if (ident->usesMd5) {
-				int x;
-				
-				NSMutableString* s = [NSMutableString string];
-				NSAutoreleasePool* p = [[NSAutoreleasePool alloc] init];
-				
-				for (x=0; x<16; x++) {
-					[s appendString: [NSString stringWithFormat: @"%02x", ident->md5Sum[x]]];
-				}
-
-				[p release];
-
-				return [NSString stringWithFormat: @"ZoomStoryID (MD5): %@", s];
-			} else {
-				return [NSString stringWithFormat: @"ZoomStoryID (nonspecific)"];
-			}
-	}
+	char* stringId = IFMB_IdToString(ident);
+	NSString* identString = [NSString stringWithUTF8String: stringId];
+	free(stringId);
+	
+	return identString;
 }
 
 @end
