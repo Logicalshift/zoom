6be8ca5bfa7c29a970c5454c7488f4c87f38a36b
Andrew Hunter <andrew@logicalshift.co.uk>
2006-09-29 20:33:30 +0000

    - now reads preferences from the ZoomPreferences object
    --HG--
    branch : zoom
    extra : convert_revision : svn%3Af2cc7feb-2fdc-0310-9003-87a4627787f1/trunk/zoom%402187

--- src/zoomCocoa/PlugIns/ZoomGlkWindowController.m
+++ src/zoomCocoa/PlugIns/ZoomGlkWindowController.m
@@ -7,8 +7,10 @@
 //
 
 #import "ZoomGlkWindowController.h"
+#import "ZoomPreferences.h"
 
 #import <GlkView/GlkHub.h>
+
 
 @implementation ZoomGlkWindowController
 
@@ -16,6 +18,39 @@
 	// Set up the Glk hub
 	[[GlkHub sharedGlkHub] useProcessHubName];
 	[[GlkHub sharedGlkHub] setRandomHubCookie];
+}
+
+// = Preferences =
+
++ (GlkPreferences*) glkPreferencesFromZoomPreferences {
+	GlkPreferences* prefs = [[GlkPreferences alloc] init];
+	ZoomPreferences* zPrefs = [ZoomPreferences globalPreferences];
+	
+	// Set the fonts according to the Zoom preferences object
+	[prefs setProportionalFont: [[zPrefs fonts] objectAtIndex: 0]];
+	[prefs setFixedFont: [[zPrefs fonts] objectAtIndex: 4]];
+	
+	// Set the foreground/background colours
+	NSColor* foreground = [[zPrefs colours] objectAtIndex: 0];
+	NSColor* background = [[zPrefs colours] objectAtIndex: 7];
+	
+	NSEnumerator* styleEnum = [[prefs styles] keyEnumerator];
+	NSMutableDictionary* newStyles = [NSMutableDictionary dictionary];
+	NSNumber* styleNum;
+
+	while (styleNum = [styleEnum nextObject]) {
+		GlkStyle* thisStyle = [[prefs styles] objectForKey: styleNum];
+		
+		[thisStyle setTextColour: foreground];
+		[thisStyle setBackColour: background];
+		
+		[newStyles setObject: thisStyle
+					  forKey: styleNum];
+	}
+	
+	[prefs setStyles: newStyles];
+	
+	return [prefs autorelease];
 }
 
 // = Initialisation =
@@ -26,12 +61,18 @@
 								  owner: self];
 	
 	if (self) {
+		[[NSNotificationCenter defaultCenter] addObserver: self
+												selector: @selector(prefsChanged:)
+													name: ZoomPreferencesHaveChangedNotification
+												  object: nil];
 	}
 	
 	return self;
 }
 
 - (void) dealloc {
+	[[NSNotificationCenter defaultCenter] removeObserver: self];
+	
 	[clientPath release];
 	[inputPath release];
 	
@@ -41,6 +82,7 @@
 - (void) maybeStartView {
 	// If we're sufficiently configured to start the application, then do so
 	if (glkView && clientPath && inputPath) {
+		[glkView setPreferences: [ZoomGlkWindowController glkPreferencesFromZoomPreferences]];
 		[glkView setInputFilename: inputPath];
 		[glkView launchClientApplication: clientPath
 						   withArguments: [NSArray array]];
@@ -53,6 +95,10 @@
 	
 	// Start it if we've got enough information
 	[self maybeStartView];
+}
+
+- (void) prefsChanged: (NSNotification*) not {
+	// TODO: actually change the preferences (might need some changes to the way Glk styles work here; styles are traditionally fixed after they are set...)
 }
 
 // = Configuring the client =
