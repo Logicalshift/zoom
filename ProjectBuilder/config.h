506e56bcaea07e83e65cb0b86be7efe7484a2b1a
Andrew Hunter <andrew@logicalshift.co.uk>
2009-09-14 14:40:56 +0000

    Updated version number to 1.1.5
    --HG--
    branch : zoom
    extra : convert_revision : svn%3Af2cc7feb-2fdc-0310-9003-87a4627787f1/trunk/zoom%403023

--- ProjectBuilder/config.h
+++ ProjectBuilder/config.h
@@ -84,7 +84,7 @@
 #define STDC_HEADERS 1
 
 /* Version number of package */
-#define VERSION "1.1.4"
+#define VERSION "1.1.5"
 
 /* Define if the X Window System is missing or not being used. */
 /* #undef X_DISPLAY_MISSING */
